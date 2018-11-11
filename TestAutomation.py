a4fec04b5f3dcb86b2f8343c67a44b89c18cf827
Alen Smailovic <alen.smailovic@gmail.com>
2018-11-11 16:12:26 +0200

    Update test with last requirements

--- TestAutomation.py
+++ TestAutomation.py
@@ -6,12 +6,12 @@ def CSC(pathOutputFile, pathOutputFileReverseCSC, width, height):
     textImage    = open(pathOutputFile, "r")
     textImageCSC = open(pathOutputFileReverseCSC, "w")
     
-    for i in range(width):
-        for j in range(height):
+    for i in range(height):
+        for j in range(width):
             # read from text file XYZ pixels
-            x = textImage.readline()
-            y = textImage.readline()
-            z = textImage.readline()
+            x = int(textImage.readline())
+            y = int(textImage.readline())
+            z = int(textImage.readline())
             
             # convert back to RGB
             r =  3.063 * x - 1.393 * y - 0.476 * z
@@ -19,10 +19,11 @@ def CSC(pathOutputFile, pathOutputFileReverseCSC, width, height):
             b =  0.068 * x - 0.229 * y + 1.069 * z
             
             # write in text file RGB pixels
-            textImageCSC.write("%.3f\n%.3f\n%.3f\n" % (r, g, b))
+            textImageCSC.write("%d\n%d\n%d\n" % (r, g, b))
     
     textImage.close()
     textImageCSC.close()
+
 
 def CompareIO(pathInputFile, pathOutputFile, width, height):
     textImageInput  = open(pathInputFile, "r")
@@ -30,65 +31,87 @@ def CompareIO(pathInputFile, pathOutputFile, width, height):
     
     diff = 0.0
     
-    for i in range(width):
-        for j in range(height):
+    for i in range(height):
+        for j in range(width):
             # read from input text file RGB pixels
-            rI = textImageInput.readline()
-            gI = textImageInput.readline()
-            bI = textImageInput.readline()
+            rI = int(textImageInput.readline())
+            gI = int(textImageInput.readline())
+            bI = int(textImageInput.readline())
             
             # read from output text file RGB pixels
-            rO = textImageOutput.readline()
-            gO = textImageOutput.readline()
-            bO = textImageOutput.readline()
+            rO = int(textImageOutput.readline())
+            gO = int(textImageOutput.readline())
+            bO = int(textImageOutput.readline())
             
             # compute the noise
             
             # compute difference
-            diff += abs(rI - rO) / 255.0
-            diff += abs(gI - gO) / 255.0
-            diff += abs(bI - bO) / 255.0
+            diff += (abs(rI - rO) / 255.0)
+            diff += (abs(gI - gO) / 255.0)
+            diff += (abs(bI - bO) / 255.0)
             
     textImageInput.close()
     textImageOutput.close()
     
     diff = diff * 100.0 / float(width * height * 3.0)
     
-    print("Difference (percentage): %.3f" % diff)
-    if diff < 10.0:
-        print("Test PASSED!")
+    print("Difference: %.2f%%" % diff)
+    if diff < 25.0:
+        print("Test PASSED")
     else:
-        print("Test FAILED!")
+        print("Test FAILED")
     
 
 def Text2Image(pathFile, width, height, pxImg):
     
-    textImage = open( pathFile, "r" )
+    textImage = open(pathFile, "r")
     
-    for x in range(width):
-        for y in range(height):
+    for x in range(height):
+        for y in range(width):
             # read from text file RGB pixels
-            r = textImage.readline()
-            g = textImage.readline()
-            b = textImage.readline()
+            r = int(textImage.readline())
+            g = int(textImage.readline())
+            b = int(textImage.readline())
          
             # get RGB pixels value from image
-            pxImg[y][x] = (r, g, b)
+            pxImg[x][y] = (r, g, b)
          
     textImage.close()
 
+
 def Image2Text( pxImg, width, height, pathFile ):
-    textImage = open( pathFile, "w" )
+    textImage = open(pathFile, "w")
     
-    for x in range(width):
-        for y in range(height):
+    for x in range(height):
+        for y in range(width):
             # get RGB pixels value from image
-            (r, g, b) = pxImg[y][x]
+            (r, g, b) = pxImg[x][y]
             
             # write in text file RGB pixels
             textImage.write("%d\n%d\n%d\n" % (r, g, b))
             
     textImage.close()
+
+
+def SaveResultImage(pathImage, width, height):
+    # Create RGB array from text file
+    oPxImg = np.zeros((height, width, 3), dtype=np.uint8)
+
+    Text2Image(pathImage, width, height, oPxImg)
+    
+    # Load the image from numpy array
+    rgb_img = Image.fromarray(oPxImg, mode = "RGB")
+    
+    # Save the image at corresponding path
+    pathOutputImage = pathImage.replace(".txt", ".bmp") 
+    rgb_img.save( pathOutputImage )
+    
+    # Open image from path
+    oImg = Image.open( pathOutputImage )
+    
+    # Show image
+    #oImg.show()
+
 
 def End2EndTest( pathImg ):
     # print the path for input image
@@ -119,35 +142,21 @@ def End2EndTest( pathImg ):
     # - don't forget the coeficients
     # - wait until is finishing
     # - *FPGA => OutputImage.txt
-    sleep(60) #sleep 60sec
+    print("Compute image on FPGA..")
+    sleep(2) #sleep 2sec
+    
+    # Save image from OutputImage
+    SaveResultImage("OutputImage.txt", width, height)
     
     # Apply the algorithm back and save the results
     CSC("OutputImage.txt", "OutputImage_ReverseCSC.txt", width, height)
     
+    # Save image from OutputImage with reverse CSC
+    SaveResultImage("OutputImage_ReverseCSC.txt", width, height)
+    
     # Compare input with output of text file -> Passed / Failed test
     CompareIO("InputImage.txt", "OutputImage_ReverseCSC.txt", width, height)
-    
-    # Create RGB array from text file
-    oPxImg = np.zeros((height, width, 3), dtype=np.uint8)
 
-    Text2Image("OutputImage.txt", width, height, oPxImg)
-    
-    # Load the image from numpy array
-    rgb_img = Image.fromarray(oPxImg, mode = "RGB")
-    
-    # Save the image at corresponding path
-    rgb_img.save( pathImg.replace(".png", "Tested.png") )
-    
-    # Open image from path
-    oImg = Image.open( pathImg )
-    
-    # Show image
-    #iImg.show()
 
 # Start test for each type of image
-End2EndTest("Images/Parrots.bmp")
-#End2EndTest("Images/VGA.png")
-#End2EndTest("Images/HD.png")
-#End2EndTest("Images/FHD.png")
-#End2EndTest("Images/QHD.png")
-#End2EndTest("Images/4K.png")
+End2EndTest("Parrots.bmp")
