737dff6fdb23febbfd20778e36f3151a01e71f90
Alen Smailović <alen.smailovic@gmail.com>
2018-12-09 19:47:54 +0200

    Finish task. Comments. Test on VM

--- Project2/WebAutomation.py
+++ Project2/WebAutomation.py
@@ -16,35 +16,52 @@ searchBox = browser.find_element_by_name("q")
 searchBox.send_keys(ARTIST)
 searchBox.submit()
 
+# Wait 5 sec (mandatory)
+sleep(5)
 
-#----
+# Get all results
+resultList = browser.find_element_by_class_name('results')
+# Get all albums from results
+albums = resultList.find_elements_by_tag_name('li')
 
-album_list = browser.find_element_by_class_name('result-items')
-albums = album_list.find_elements_by_tag_name('li')
-
+# Iterate through all albums
 for album in albums:
+	# Make sure type is album
 	if album.find_element_by_class_name('itemtype').text.find('ALBUM') != -1:
+		# Get album name
 		albumName = album.find_element_by_class_name('heading')
-		print (albumName.text)
-		if albumName.text.find('Best Of Lady Gaga') != -1:
-			link = album.find_element_by_link_text(albumName.text)
-			link.click()
+		# Check if it's our album
+		if albumName.text.find(ALBUM) != -1:
+			print("Album found..")
+			# Get link of album and go into
+			albumLink = album.find_element_by_link_text(albumName.text)
+			albumLink.click()
 			break
 
+# Wait 5 sec (mandatory)
+sleep(5)
 
-track_list = browser.find_element_by_id('track_table')
-tracks = track_list.find_elements_by_class_name('track_row_view')
+# Get tracks table from results
+trackTable = browser.find_element_by_id('track_table')
+# Get all tracks
+tracks = trackTable.find_elements_by_class_name('track_row_view')
 
+# Iterate through all tracks
 for track in tracks:
+	# Get track name
 	trackName = track.find_element_by_class_name('title')
-	print(trackName.find_element_by_tag_name('a').text)
-	if trackName.text.find('Judas') != -1:
+	# Check if it's out track
+	if trackName.text.find(TRACK) != -1:
+		print("Track found..")
+		# Play music :)
 		playButton = track.find_element_by_class_name('play-col')
 		playButton.click()
+		print("Play music! :)")
 		break
 
-
+# Wait 10 sec
 sleep(10)
 
-pauseButton = browser.find_element_by_class_name('playbutton')
-pauseButton.click()
+# Exit browser
+browser.close()
+print("Exit")
